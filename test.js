a5e032fa72dc9b82470f27bcac7e047ff548818c
shaheer1642 <shaheer1642@gmail.com>
2023-06-25 23:20:18 +0500

    deployment fix

---
+++ test.js
@@ -0,0 +1,22 @@
+
+
+fetch('https://w7.pngwing.com/pngs/817/902/png-transparent-google-logo-google-doodle-google-search-google-company-text-logo-thumbnail.png').then(async response => {
+    const blob = await response.arrayBuffer();
+    const contentType = response.headers.get('content-type');
+    const base64String = `${Buffer.from(
+      blob,
+    ).toString('base64')}`;
+
+    console.log(base64String)
+    
+    fetch('http://192.168.18.25:5000/predict', {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({image: base64String})
+    }).then(res => res.json()).then(res => {
+        console.log(res)
+    })
+}).catch(console.error)
